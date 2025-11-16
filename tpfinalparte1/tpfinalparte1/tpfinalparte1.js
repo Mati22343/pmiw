6b48d8befa3ef1e70057fa6d9fcfc55701bd4975
Mati22343 <matygamer51@gmail.com>
2025-11-16 06:49:24 -0300

    Update tpfinalparte1.js

--- tpfinalparte1/tpfinalparte1/tpfinalparte1.js
+++ tpfinalparte1/tpfinalparte1/tpfinalparte1.js
@@ -1,5 +1,5 @@
 //comision 1 - Matias Arias Julia Miccino
-// https://www.youtube.com/watch?v=EjiXZ3iGxf8
+// https://youtu.be/XU-5EhznfEc
 
 let pantallaActual = 0;
 let pantallas = [];
@@ -161,3 +161,4 @@ function mousePressed() {
         click.play();
     }
 }
+
